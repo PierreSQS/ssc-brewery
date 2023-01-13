4425aa05a599b12b32b690ad45ac052cb6dde0a1
PierreSQS <nomo_messina@web.de>
2023-01-14 00:38:09 +0100

    Sec7_Chap44-Spring Security Config.-NOK: remove the default security setting

--- src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
@@ -12,8 +12,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-
 /**
  * Created by jt on 6/13/20.
  */
@@ -42,7 +40,6 @@ public abstract class BaseIT {
     public void setup() {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(wac)
-                .apply(springSecurity())
                 .build();
     }
 }
