d9bb98f00c68f79b5d991f809e7b03346b517cc0
PierreSQS <nomo_messina@web.de>
2023-01-16 15:12:08 +0100

    Sec8_Chap53-H2 DB Console Access-NOK: myTemp Solution

--- src/main/java/guru/sfg/brewery/security/AbstractRestAuthFilter.java
+++ src/main/java/guru/sfg/brewery/security/AbstractRestAuthFilter.java
@@ -1,13 +1,10 @@
 package guru.sfg.brewery.security;
 
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -15,7 +12,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
-import java.io.IOException;
 
 /**
  * Created by jt on 6/20/20.
@@ -25,48 +21,6 @@ public abstract class AbstractRestAuthFilter extends AbstractAuthenticationProce
 
     protected AbstractRestAuthFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
         super(requiresAuthenticationRequestMatcher);
-    }
-
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-            throws IOException, ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-
-
-        if (logger.isDebugEnabled()) {
-            logger.debug("Request is to process authentication");
-        }
-
-        try {
-            Authentication authResult = attemptAuthentication(request, response);
-
-            if (authResult != null) {
-                successfulAuthentication(request, response, chain, authResult);
-            } else {
-                chain.doFilter(request, response);
-            }
-        } catch (AuthenticationException e) {
-            log.error("Authentication Failed", e);
-            unsuccessfulAuthentication(request, response, e);
-        }
-    }
-
-    @Override
-    protected void unsuccessfulAuthentication(HttpServletRequest request,
-                                              HttpServletResponse response, AuthenticationException failed)
-            throws IOException {
-
-        SecurityContextHolder.clearContext();
-
-        if (log.isDebugEnabled()) {
-            log.debug("Authentication request failed: " + failed.toString(), failed);
-            log.debug("Updated SecurityContextHolder to contain null Authentication");
-        }
-
-        response.sendError(HttpStatus.UNAUTHORIZED.value(),
-                HttpStatus.UNAUTHORIZED.getReasonPhrase());
     }
 
     @Override
@@ -89,14 +43,13 @@ public abstract class AbstractRestAuthFilter extends AbstractAuthenticationProce
         if (userName.equals("spring") && password.equals("guru")) {
             return this.getAuthenticationManager().authenticate(token);
         } else {
-            return null;
+//            return null;
+            throw new AuthenticationCredentialsNotFoundException("Api Key and Secret not submitted!!!");
         }
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response, FilterChain chain, Authentication authResult) {
-
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
         if (logger.isDebugEnabled()) {
             logger.debug("Authentication success. Updating SecurityContextHolder to contain: "
                     + authResult);
