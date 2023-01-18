45d5a980a939e2cd48ec79906044a98f0b151d4a
PierreSQS <nomo_messina@web.de>
2023-01-18 14:48:22 +0100

    Sec7_Chap46-Custom Do Filter-NOK-V2: added doFilter()-Method to meet the filter requirements

--- src/main/java/guru/sfg/brewery/security/RestHeaderAuthFilter.java
+++ src/main/java/guru/sfg/brewery/security/RestHeaderAuthFilter.java
@@ -1,14 +1,21 @@
 package guru.sfg.brewery.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.util.StringUtils;
+
+import java.io.IOException;
 
 /**
  * Modified by Pierrot on 1/18/2023
@@ -18,6 +25,46 @@ public class RestHeaderAuthFilter extends AbstractAuthenticationProcessingFilter
 
     public RestHeaderAuthFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
         super(requiresAuthenticationRequestMatcher);
+    }
+
+    /**
+     * The Requirements of the overwritten Implementation:
+     <ol>
+     * <li> we authenticate with the headers Api-Keys if available</li>
+     * <li> if not we continue the filter chain.
+     * </ol>
+     * @param request: the request sent
+     * @param response: the response received
+     * @param chain: the filter chain
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpReq = (HttpServletRequest) request;
+        HttpServletResponse httpResp = (HttpServletResponse) response;
+
+        if (!requiresAuthentication(httpReq, httpResp)) {
+            chain.doFilter(httpReq, response);
+            return;
+        }
+        try {
+            Authentication authenticationResult = attemptAuthentication(httpReq, httpResp);
+            if (authenticationResult != null) {
+                successfulAuthentication(httpReq, httpResp, chain, authenticationResult);
+            } else {
+                chain.doFilter(request,response);
+            }
+
+        }
+        catch (InternalAuthenticationServiceException failed) {
+            this.logger.error("An internal error occurred while trying to authenticate the user.", failed);
+            unsuccessfulAuthentication(httpReq, httpResp, failed);
+        }
+        catch (AuthenticationException ex) {
+            // Authentication failed
+            unsuccessfulAuthentication(httpReq, httpResp, ex);
+        }
     }
 
     @Override
