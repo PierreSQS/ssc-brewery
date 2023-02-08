922a8abdee9b5267858edc3fbeaf99689b0c7e10
PierreSQS <nomo_messina@web.de>
2023-02-08 15:16:33 +0100

    Sec13_Chap89-Add Login Form: fixed one test

--- src/main/java/guru/sfg/brewery/web/controllers/BeerController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/BeerController.java
@@ -24,6 +24,7 @@ import guru.sfg.brewery.repositories.BeerRepository;
 import guru.sfg.brewery.security.perms.BeerCreatePermission;
 import guru.sfg.brewery.security.perms.BeerReadPermission;
 import guru.sfg.brewery.security.perms.BeerUpdatePermission;
+import guru.sfg.brewery.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -95,6 +96,7 @@ public class BeerController {
     @GetMapping("/new")
     public String initCreationForm(Model model) {
         model.addAttribute("beer", Beer.builder().build());
+        model.addAttribute("beerStyle", BeerStyleEnum.values());
         return "beers/createBeer";
     }
 
