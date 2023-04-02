8bd7508886e22dabe36d1d044a48e8639c5bfe64
PierreSQS <nomo_messina@web.de>
2023-04-02 20:29:02 +0200

    Sec9_Chap60-Verifying Role-NOK: related code

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,7 +19,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Order(1)
+    public SecurityFilterChain generalFilterChain(HttpSecurity http) throws Exception {
 
                 http
                     .authorizeHttpRequests(authorize -> authorize
@@ -38,6 +40,13 @@ public class SecurityConfig {
                 //h2 console config
                 http.headers().frameOptions().sameOrigin();
                 return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain rolesFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.DELETE,"/api/v1/beer/**").hasRole("ADMIN");
+        return httpSecurity.build();
     }
 
     @Bean
--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -6,24 +6,48 @@ import org.junit.jupiter.api.Test;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Modified by Pierrot on 2023-01-22.
+ * Extended by Pierrot on 2023-04-01.
  */
-public class BeerRestControllerIT extends BaseIT {
+class BeerRestControllerIT extends BaseIT {
 
     @Test
     void deleteBeerHttpBasic() throws Exception{
         mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
-                .with(httpBasic("spring", "guru")))
-                .andExpect(status().is2xxSuccessful());
+                        .with(httpBasic("spring", "guru")))
+                .andExpect(status().is2xxSuccessful())
+                .andDo(print());
+    }
+    @Test
+    void deleteBeerHttpBasicToDelete() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                        .with(httpBasic("spring", "guru")))
+                .andExpect(status().is2xxSuccessful())
+                .andDo(print());
     }
 
     @Test
     void deleteBeerNoAuth() throws Exception{
         mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void deleteBeerHttpUserRole() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                        .with(httpBasic("user", "password")))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void deleteBeerHttpCustomerRole() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                        .with(httpBasic("scott", "tiger")))
+                .andExpect(status().isForbidden())
+                .andDo(print());
     }
 
     @Test
