d15ee2e0737302b9e2ed965bf41030862feffc72
PierreSQS <nomo_messina@web.de>
2023-04-10 12:45:43 +0200

    Sec9_Chap60-Verifying: fixed the listBeers() issue

--- src/main/java/guru/sfg/brewery/web/model/BaseItem.java
+++ src/main/java/guru/sfg/brewery/web/model/BaseItem.java
@@ -17,7 +17,6 @@
 
 package guru.sfg.brewery.web.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -38,11 +37,9 @@ public class BaseItem {
     @JsonProperty("version")
     private Integer version = null;
 
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     @JsonProperty("createdDate")
     private LocalDateTime createdDate = null;
 
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     @JsonProperty("lastModifiedDate")
     private LocalDateTime lastModifiedDate = null;
 }
