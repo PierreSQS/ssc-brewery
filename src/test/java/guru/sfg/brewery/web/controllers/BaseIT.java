d8e9c3e6246b2e613dd6e4c6cbfc15764b5c3d4c
PierreSQS <nomo_messina@web.de>
2023-04-20 17:14:13 +0200

    Sec9_Chap63-Introduction to Method Security: alternative to previous commit

--- src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
@@ -4,31 +4,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.provider.Arguments;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.stream.Stream;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-
 /**
- * Created by jt on 6/13/20.
+ * Modified by Pierrot on 4/20/23.
  */
 @Import({H2ConsoleProperties.class})
+@AutoConfigureMockMvc
 public abstract class BaseIT {
-    @Autowired
-    WebApplicationContext wac;
 
-    protected MockMvc mockMvc;
+    @Autowired protected MockMvc mockMvc;
 
     @BeforeEach
     public void setup() {
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(wac)
-                .apply(springSecurity())
-                .build();
+
     }
 
     public static Stream<Arguments> getStreamAdminCustomer() {
