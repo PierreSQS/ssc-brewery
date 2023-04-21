b8fcf9f34f9bacb730d1f88b8289805b9c1dc0d9
PierreSQS <nomo_messina@web.de>
2023-04-22 00:03:52 +0200

    Sec9_Chap64-Security Expression Based Syntax: code cleaning

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -20,26 +20,25 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
                 http
-                .authorizeHttpRequests(authorize -> {
-                    authorize
-                            .requestMatchers("/h2-console/**").permitAll() //do not use in production!
-                            .requestMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
-                            .requestMatchers(HttpMethod.GET, "/api/v1/beer/**")
-                                .hasAnyRole("ADMIN", "CUSTOMER", "USER")
-                            .requestMatchers(HttpMethod.DELETE, "/api/v1/beer/**").hasRole("ADMIN")
-                            .requestMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}")
-                                .hasAnyRole("ADMIN", "CUSTOMER", "USER")
-                            .requestMatchers("/brewery/breweries")
-                                .hasAnyRole("ADMIN", "CUSTOMER")
-                            .requestMatchers(HttpMethod.GET, "/brewery/api/v1/breweries")
-                                .hasAnyRole("ADMIN", "CUSTOMER")
-                            .requestMatchers("/beers/find", "/beers/{beerId}")
-                                .hasAnyRole("ADMIN", "CUSTOMER", "USER");
-                } )
-                .authorizeHttpRequests().anyRequest().authenticated()
+                   .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/h2-console/**").permitAll() //do not use in production!
+                        .requestMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/beer/**")
+                            .hasAnyRole("ADMIN", "CUSTOMER", "USER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/beer/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}")
+                            .hasAnyRole("ADMIN", "CUSTOMER", "USER")
+                        .requestMatchers("/brewery/breweries")
+                            .hasAnyRole("ADMIN", "CUSTOMER")
+                        .requestMatchers(HttpMethod.GET, "/brewery/api/v1/breweries")
+                            .hasAnyRole("ADMIN", "CUSTOMER")
+                        .requestMatchers("/beers/find", "/beers/{beerId}")
+                            .hasAnyRole("ADMIN", "CUSTOMER", "USER"))
+                   .authorizeHttpRequests().anyRequest().authenticated()
                 .and()
-                    .formLogin().and()
-                .httpBasic()
+                    .formLogin()
+                .and()
+                    .httpBasic()
                 .and()
                     .csrf().disable();
 
