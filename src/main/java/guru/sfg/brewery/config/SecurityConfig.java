5768b52a8c13ebcd99b7a3b254ed0c756d976d9e
PierreSQS <nomo_messina@web.de>
2023-04-21 17:45:11 +0200

    Sec9_Chap64-Security Expression Based Syntax-NOK: corrected the Sec. Config

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
                 http
@@ -51,53 +52,5 @@ public class SecurityConfig {
     PasswordEncoder passwordEncoder(){
         return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-
-   // @Override
- //   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       // auth.userDetailsService(this.jpaUserDetailsService).passwordEncoder(passwordEncoder());
-
-//        auth.inMemoryAuthentication()
-//                .withUser("spring")
-//                .password("{bcrypt}$2a$10$7tYAvVL2/KwcQTcQywHIleKueg4ZK7y7d44hKyngjTwHCDlesxdla")
-//                .roles("ADMIN")
-//                .and()
-//                .withUser("user")
-//                .password("{sha256}1296cefceb47413d3fb91ac7586a4625c33937b4d3109f5a4dd96c79c46193a029db713b96006ded")
-//                .roles("USER");
-//
-//        auth.inMemoryAuthentication().withUser("scott").password("{bcrypt15}$2a$15$baOmQtw8UqWZRDQhMFPFj.xhkkWveCTQHe4OBdr8yw8QshejiSbI6").roles("CUSTOMER");
-  //  }
-
-    //    @Override
-//    @Bean
-//    protected UserDetailsService userDetailsService() {
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("spring")
-//                .password("guru")
-//                .roles("ADMIN")
-//                .build();
-//
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, user);
-//    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
