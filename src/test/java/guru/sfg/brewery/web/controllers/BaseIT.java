f2ce04297cfd9b8d6cc1723b029cae30adeee9ef
PierreSQS <nomo_messina@web.de>
2023-04-23 17:52:35 +0200

    Sec10_Chap67-Updated JPA Configuration-NOK: fixed the H2ConsoleProperties Issue

--- src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
@@ -3,6 +3,8 @@ package guru.sfg.brewery.web.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.provider.Arguments;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -12,8 +14,9 @@ import java.util.stream.Stream;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 /**
- * Created by jt on 6/13/20.
+ * Modified by Pierrot on 2023-04-23.
  */
+@Import(H2ConsoleProperties.class)
 public abstract class BaseIT {
     @Autowired
     WebApplicationContext wac;
