5e2f31e2805468ee0893ca643b6a7f5852b818fe
PierreSQS <nomo_messina@web.de>
2023-05-01 21:06:46 +0200

    Sec10_Assn10-Update Remaining Controllers: introduced Security User

--- src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
+++ src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
@@ -1,25 +1,18 @@
 package guru.sfg.brewery.security;
 
-import guru.sfg.brewery.domain.security.Authority;
+import guru.sfg.brewery.domain.security.SecurityUser;
 import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 /**
- * Created by jt on 6/22/20.
+ * Modified by Pierrot on 5/1/23.
  */
 @Slf4j
 @RequiredArgsConstructor
@@ -37,19 +30,7 @@ public class JpaUserDetailsService implements UserDetailsService {
         User user = userRepository.findByUsername(username).orElseThrow(() ->
                 new UsernameNotFoundException("User name: " + username + " not found"));
 
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-                user.getEnabled(), user.getAccountNonExpired(), user.getCredentialsNonExpired(),
-                user.getAccountNonLocked(), convertToSpringAuthorities(user.getAuthorities()));
+        return new SecurityUser(user);
     }
 
-    private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
-        if (authorities != null && !authorities.isEmpty()){
-            return authorities.stream()
-                    .map(Authority::getPermission)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toSet());
-        } else {
-            return new HashSet<>();
-        }
-    }
 }
