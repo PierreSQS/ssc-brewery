6394531c7a7541362a91fed5591d1c50866b2de3
PierreSQS <nomo_messina@web.de>
2023-05-01 16:23:12 +0200

    Sec10_Assn10-Update Remaining Controllers: Pierrot assn10 related code

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,22 +19,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
-    private static final String ADMIN_ROLE = "ADMIN";
-    private static final String CUSTOMER_ROLE = "CUSTOMER";
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
                 http
                    .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(PathRequest.toH2Console()).permitAll() //do not use in production!
-                        .requestMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
-                        .requestMatchers("/brewery/breweries")
-                            .hasAnyRole(ADMIN_ROLE, CUSTOMER_ROLE)
-                        .requestMatchers(HttpMethod.GET, "/brewery/api/v1/breweries")
-                            .hasAnyRole(ADMIN_ROLE, CUSTOMER_ROLE)
-                        .requestMatchers("/beers/find", "/beers/{beerId}")
-                            .hasAnyRole(ADMIN_ROLE, CUSTOMER_ROLE, "USER"))
+                        .requestMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll())
                    .authorizeHttpRequests().anyRequest().authenticated()
                 .and()
                     .formLogin()
--- src/main/java/guru/sfg/brewery/web/controllers/CustomerController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/CustomerController.java
@@ -49,7 +49,7 @@ public class CustomerController {
         return "customers/findCustomers";
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'CUSTOMER')")
+    @PreAuthorize("hasAnyAuthority('customer.create', 'customer.update')")
     @GetMapping
     public String processFindFormReturnMany(Customer customer, BindingResult result, Model model){
         // find customers by name
@@ -85,7 +85,7 @@ public class CustomerController {
         return "customers/createCustomer";
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAnyAuthority('customer.create')")
     @PostMapping("/new")
     public String processCreationForm(Customer customer) {
         //ToDO: Add Service
