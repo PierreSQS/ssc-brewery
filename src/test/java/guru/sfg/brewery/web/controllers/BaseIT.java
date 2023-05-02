2dcdfdc95bded4b5c0df0845ff949a3a82a73dfa
PierreSQS <nomo_messina@web.de>
2023-05-02 16:09:38 +0200

    Sec11_Chap73-Order Contr. Code Review: optimized IT-Tests

--- src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
@@ -3,33 +3,26 @@ package guru.sfg.brewery.web.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.provider.Arguments;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
 import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.stream.Stream;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-
 /**
- * Created by jt on 6/13/20.
+ * Modified by Pierrot 5/1/23.
  */
+@AutoConfigureMockMvc
 @Import(H2ConsoleProperties.class)
 public abstract class BaseIT {
-    @Autowired
-    WebApplicationContext wac;
 
+    @Autowired
     protected MockMvc mockMvc;
 
     @BeforeEach
     public void setup() {
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(wac)
-                .apply(springSecurity())
-                .build();
+
     }
 
     public static Stream<Arguments> getStreamAdminCustomer() {
